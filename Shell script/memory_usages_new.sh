ea005790ed01fdf5c1621a65884e54eadc3cedcc
devopsgittesting <85344211+devopsgittesting@users.noreply.github.com>
2021-09-17 10:51:12 +0530

    Add files via upload
    checking memory usages for every 1 min continue till 5 min , based on last min it will decide memory usages high or low if high then it will sent mail to master node

---
+++ Shell script/memory_usages_new.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+mem_threshold='10'
+i=0;
+
+#set -x
+
+while [ $i -le 6 ]
+do
+
+
+mem_usage=`free -m | awk 'NR==2 {printf $3*100/$2}' | cut -f 1 -d "."`
+
+
+       
+
+        if [[ $i -ge 6 && $mem_usage -gt $mem_threshold ]]
+          
+        then
+                  
+          echo "mem warning!!!"
+          echo "memory usages high :$mem_usage%"
+          echo "Sending mail to Master Node "
+          echo "Memory usages is high $mem_usage%" | mail -s "Memory Usages Alert" ansible@docker.example.com
+          echo $mem_usage memory
+          exit 0 
+       else
+          echo "memory usages :$mem_usage%"
+          echo "mem ok!!!"
+           
+         fi
+sleep 5   
+
+echo Number: $i
+((i++))
+
+done
