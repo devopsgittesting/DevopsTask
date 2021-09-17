d6cbb91907906883e7b0896f194786fe44f68a07
devopsgittesting <85344211+devopsgittesting@users.noreply.github.com>
2021-09-17 10:56:02 +0530

    Update file.sh

--- Shell script/file.sh
+++ Shell script/file.sh
@@ -1,4 +1,9 @@
-
+#!/bin/bash
+#Problem stmt when extracting tar file i don't want to override on existing data or folder 
+#sol- when extracting it will automatically override with existing data 
+#we can exclude that folder or directory while extracting if folder already present then we will skip or exclude that folder
+#and extract remaining folder 
+#if that folder not present then extract all folders
 
 Tarfile=sample.tar.gz
 
